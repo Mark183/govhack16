786f4192bcccd8f5f2938811e205ace3e76133f7
Daniel Farmer <faaarmer@gmail.com>
2016-07-31 12:05:09 +1000

    removed the radius from the map!

--- src/assets/js/resultMap.js
+++ src/assets/js/resultMap.js
@@ -92,16 +92,6 @@ var Results = {
 	                animation: google.maps.Animation.DROP
 	              });
 			})
-			var cityCircle = new google.maps.Circle({
-              strokeColor: '#FF0000',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#FF0000',
-              fillOpacity: 0.35,
-              map: Results.map,
-              center: {lat: Locations[Results.location].lat, lng: Locations[Results.location].lng},
-              radius: Locations[Results.location].distance * 1000
-            });
 		}
 	},
 
