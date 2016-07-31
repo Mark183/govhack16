f24a70464aedc76d48794cdefbca73a0749a6a10
Mark Ganser <Mark@Marks-MacBook-Pro.local>
2016-07-31 16:05:58 +1000

    put touched on map button for iOS

--- src/assets/js/view.js
+++ src/assets/js/view.js
@@ -4,8 +4,8 @@ var View = {
 	},
 
 	events: function(){
-		$('body').on('click touch', '#list_view', View.showListView);
-		$('body').on('click touch', '#map_view', View.showMapView);
+		$('body').on('click touchend', '#list_view', View.showListView);
+		$('body').on('click touchend', '#map_view', View.showMapView);
 	},
 
 	showListView: function() {
